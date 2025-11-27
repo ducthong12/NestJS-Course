fd87efecad5f518fc247f749ad7e0693e5fd5d72
Nguyen Duc Thong <thongnd@v-acs.net>
2025-11-27 10:54:08 +0700

    #1 Setup Kafka

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,29 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Attach Kafka microservice to the HTTP application
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092', 'localhost:9093', 'localhost:9094'],
+      },
+      consumer: {
+        groupId: 'user-consumer-group', // Quan trọng: Định danh nhóm consumer
+      },
+      run: {
+        autoCommit: false,
+      },
+    },
+  });
+
+  // Start microservices before starting the HTTP server
+  await app.startAllMicroservices();
   app.useGlobalPipes(new ValidationPipe());
   //Enable cors
   app.enableCors();
