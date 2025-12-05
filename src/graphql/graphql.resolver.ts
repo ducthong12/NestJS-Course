bd1bdcf4853479fa75634ed6a0ac01cc87d00785
Nguyen Duc Thong <thongnd@v-acs.net>
2025-12-05 09:37:23 +0700

    Fix source

--- src/graphql/graphql.resolver.ts
+++ src/graphql/graphql.resolver.ts
@@ -1,14 +1,27 @@
-import { Args, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+  Subscription,
+} from '@nestjs/graphql';
 import { GraphqlService } from './graphql.service';
 import { PraticePrismaService } from 'src/pratice_prisma/pratice_prisma.service';
-import { CreateUserInput, UpdateUserInput, UserOutput } from './dto/graphql';
+import {
+  CreateUserInput,
+  UpdateUserInput,
+  UserOutput,
+  UserWithPosts,
+} from './dto/graphql';
 import { RedisPubSub } from 'graphql-redis-subscriptions';
 import { Inject } from '@nestjs/common';
 import { PUB_SUB } from 'src/common/pubsub.module';
 
 const EVENT_USER_ADDED = 'userAdded';
 
-@Resolver(() => UserOutput)
+@Resolver(() => UserWithPosts)
 export class GraphqlResolver {
   constructor(
     private readonly graphqlService: GraphqlService,
@@ -16,17 +29,12 @@ export class GraphqlResolver {
     @Inject(PUB_SUB) private readonly pubSub: RedisPubSub,
   ) {}
 
-  // @Mutation(() => UserOutput)
-  // createUser(@Args('createUserInput') createUserInput: CreateUserDto) {
-  //   return this.praticePrismaService.create(createUserInput);
-  // }
-
   @Query(() => [UserOutput], { name: 'findAllUser', nullable: true })
   findAll() {
     return this.praticePrismaService.findAll();
   }
 
-  @Query(() => [UserOutput], { nullable: true })
+  @Query(() => [UserWithPosts], { nullable: true })
   findUserWithLeastPost() {
     return this.praticePrismaService.findUserWithLeastPost();
   }
@@ -50,8 +58,7 @@ export class GraphqlResolver {
 
   @Mutation(() => UserOutput)
   createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-    this.pubSub.publish(EVENT_USER_ADDED, {
+    void this.pubSub.publish(EVENT_USER_ADDED, {
       userAdded: createUserInput,
     });
     return this.graphqlService.create(createUserInput);
@@ -60,7 +67,12 @@ export class GraphqlResolver {
   @Subscription(() => UserOutput)
   userAdded() {
     // Logic to handle userAdded subscription
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
     return this.pubSub.asyncIterator(EVENT_USER_ADDED);
+  }
+
+  @ResolveField('countUser', () => Number)
+  getCountUser(@Parent() findUserWithLeastPost: UserWithPosts) {
+    console.log('Parent:', findUserWithLeastPost);
+    return this.praticePrismaService.getCountByUser();
   }
 }
